fa8df2b53bfe29db7984e6730154baf856749088
Ruslan <ruslan.dev.box@gmail.com>
2021-11-18 18:28:26 +0500

    Minor fix

--- src/singerly_airflow/dag_generator.py
+++ src/singerly_airflow/dag_generator.py
@@ -22,6 +22,6 @@ def build_dag(pipeline: Pipeline) -> DAG:
     start >> singerly_task >> end
   return dag
 
-def build_dags(project_id: str):
+def build_dags(project_id: str, globals):
   for pipeline in get_pipelines(project_id):
-    globals()[pipeline.id] = build_dag(pipeline)
+    globals[pipeline.id] = build_dag(pipeline)
