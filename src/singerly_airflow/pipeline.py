458be94011dc80a3fd8c07ac8322d218d3c7b5e8
Ruslan <ruslan.dev.box@gmail.com>
2022-01-05 11:55:53 +0500

    Minor fix

--- src/singerly_airflow/pipeline.py
+++ src/singerly_airflow/pipeline.py
@@ -103,7 +103,7 @@ class Pipeline:
     if stdout_decoded:
       print(stdout_decoded)
       self.save_state(stdout_decoded)
-    if stderr.decode('utf-8'):
+    if stderr and stderr.decode('utf-8'):
       print(stderr.decode('utf-8'))
 
   def is_valid(self) -> bool:
