2ce57f37fda81210a651c051774fb3516a7d5d6c
Ruslan <ruslan.dev.box@gmail.com>
2022-02-07 17:35:36 +0500

    Minor fixes

--- src/singerly_airflow/operator.py
+++ src/singerly_airflow/operator.py
@@ -7,8 +7,8 @@ import asyncio
 
 
 class SingerlyOperator(BaseOperator):
-    def __init__(self, pipeline_id: str, **kwargs) -> None:
-        super().__init__(**kwargs)
+    def __init__(self, pipeline_id: str, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.pipeline_id = pipeline_id
 
     def execute(self, context: Context):
@@ -18,3 +18,4 @@ class SingerlyOperator(BaseOperator):
         if pipeline and pipeline.is_valid():
             pipeline_executor = Executor(pipeline)
             asyncio.run(pipeline_executor.execute())
+            print("Finished execution")
